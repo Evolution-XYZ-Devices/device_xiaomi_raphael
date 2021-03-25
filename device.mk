46578725bf25ce6c2b09d11c6f1889cf2ed47803
Edwin Wong <edwinwong@google.com>
2021-03-25 14:36:24 -0700

    raphael: Update clearkey plugin to drm HAL v1.4
    Test: atest
      atest CtsMediaTestCases:android.media.cts.MediaDrmClearkeyTest#testGetPlaybackComponent
    
    Bug: 182525516
    Change-Id: I406b7d3c2921d4698535421894c6eb3785eb1c0d

--- device.mk
+++ device.mk
@@ -206,7 +206,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 # Fingerprint
 PRODUCT_PACKAGES += \
