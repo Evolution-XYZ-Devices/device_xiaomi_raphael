4e9467a7289451b341010f21fd719f1dc6bb18ea
EcrosoftXiao <xy19990903@gmail.com>
2021-05-30 00:15:14 +0600

    raphael: Switch to Thermal 2.0 mock
    * QTI Thermal couldn't read the temperature for some reason
    * sepolicy: Adjust sepolicy for thermal
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- device.mk
+++ device.mk
@@ -466,7 +466,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal@2.0-service.mock
 
 # USB
 PRODUCT_PACKAGES += \
