3d9f2744a4a1428aeae62088d7f14d7815a37491
TH779 <i@779.moe>
2021-10-14 15:37:47 +0000

    raphael: Uprev fingerprint HAL to 2.3
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: Ia36a9f6f2ed866824a95c2763eae4da15293c448

--- device.mk
+++ device.mk
@@ -210,7 +210,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_raphael \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_raphael \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_raphael
 
 # FM
