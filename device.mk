d2b92595f9e83b6628ff55a1f07bf82f39aa6e96
zaidkhan0997 <kzaid0997@gmail.com>
2021-12-13 19:08:41 +0200

    raphael: Build necessary camera libs
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- device.mk
+++ device.mk
@@ -156,7 +156,11 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     libgui_vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor \
     vendor.xiaomi.hardware.motor@1.0.vendor
+
+PRODUCT_PACKAGES += \
+    libstdc++.vendor
 
 PRODUCT_PACKAGES += \
     Snap
