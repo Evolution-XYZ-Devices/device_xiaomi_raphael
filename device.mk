8c5355da4a8f68cc6868e80e379ddf5a31495a6c
kondors1995 <normandija1945@gmail.com>
2022-01-22 22:00:45 +0000

    raphael: Move to caf thermal
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- device.mk
+++ device.mk
@@ -452,6 +452,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtinyxml2
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
