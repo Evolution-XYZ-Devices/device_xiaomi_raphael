05597110716ccd6dc3413e3cf22cbfd8ee23d4e3
Jabiyeff <cebiyevanar@gmail.com>
2022-03-02 18:15:27 +0900

    raphael: Remove power-stats HAL support for now
    * It doesn't work stable on Android 12
    
    11-03 00:38:51.734   581   581 E android.hardware.power.stats@1.0-service-mock: Failed to getEnergyData
    
    Revert "raphael: Build mock power stats service"
    
    This reverts commit 596ad45b3253e4d89860ce8ab06eabbe390ba07f.

--- device.mk
+++ device.mk
@@ -398,8 +398,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2 \
     android.hardware.power@1.2.vendor \
-    android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power-service-qti
 
 # QMI
 PRODUCT_PACKAGES += \
