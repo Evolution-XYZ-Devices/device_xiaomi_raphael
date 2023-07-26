4efc4d5150c50cf45ddb68796bf567f146fe81d5
kondors1995 <normandija1945@gmail.com>
2023-07-26 15:29:30 +0000

    raphael: Add Framework Compatibility Matrix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,6 +81,7 @@ BOARD_HAVE_QCOM_FM := true
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/hidl/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/hidl/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/hidl/framework_compatibility_matrix.xml
 ODM_MANIFEST_SKUS += nfc nfc_ese
 ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/hidl/manifest_nfc.xml
 ODM_MANIFEST_NFC_ESE_FILES := \
