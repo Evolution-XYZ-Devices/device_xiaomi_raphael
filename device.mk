4bac8d8574b6e3e464b9e5b52f481ab286e05feb
John Galt <johngaltfirstrun@gmail.com>
2023-10-13 09:50:45 -0400

    raphael: move to AIDL wifi

--- device.mk
+++ device.mk
@@ -570,7 +570,7 @@ PRODUCT_PRODUCT_VNDK_VERSION := current
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
