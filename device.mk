2770733b6735f61e34754aee24526efb3ad24408
RealJohnGalt <johngaltfirstrun@gmail.com>
2023-10-28 12:22:25 +0300

    raphael: protobuf vendor compat
    Our R prebuilts use protobuf 3.9.1 causing many prebuilts to fail

--- device.mk
+++ device.mk
@@ -433,6 +433,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/etc/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
