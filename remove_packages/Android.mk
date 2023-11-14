1b172604bf25826bc01480c953eb768014f04f07
Joey Huab <joey@evolution-x.org>
2023-11-15 00:16:03 +0900

    raphael: Drop more prebuilt gapps

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,8 +4,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := PixelLiveWallpaperPrebuilt
+LOCAL_OVERRIDES_PACKAGES := CalendarGooglePrebuilt DevicePolicyPrebuilt GoogleTTS Photos PixelLiveWallpaperPrebuilt
 LOCAL_OVERRIDES_PACKAGES += PixelWallpapers2020 PixelWallpapers2021 PixelWallpapers2022 PixelWallpapers2022a PixelWallpapers2023Tablet
+LOCAL_OVERRIDES_PACKAGES += PixelWallpapers2023 RecorderPrebuilt SoundAmplifierPrebuilt Videos
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
