062e732d79fd669127c245c3528f9c39f4532bb3
romgharti <romgharti@gmail.com>
2023-11-19 12:51:46 +0545

    raphael: Build android.hardware.audio.sounddose-vendor-impl
    E audiohalservice: Failed to dlopen android.hardware.audio.sounddose-vendor-impl.so: dlopen failed: library android.hardware.audio.sounddose-vendor-impl.so not found
    
    Signed-off-by: romgharti <romgharti@gmail.com>

--- device.mk
+++ device.mk
@@ -106,6 +106,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-util \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
+    android.hardware.audio.sounddose-vendor-impl \
     android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.bluetooth.default \
